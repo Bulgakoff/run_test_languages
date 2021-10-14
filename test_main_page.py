034659308d07f94e831b5126187203bc5b9310b0
Alex <hlbu@yandex.ru>
2021-10-14 21:19:13 +0300

    simple foo_1

--- test_main_page.py
+++ test_main_page.py
@@ -1,10 +1,15 @@
 import time
 
+link = "http://selenium1py.pythonanywhere.com/"
 
-def test_guest_can_go_to_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
-    browser.get(link)
-    time.sleep(20)
+
+def go_to_login_page(browser):
     login_link = browser.find_element_by_css_selector("#login_link")
     print(f'============================= {login_link.text}')
     login_link.click()
+
+
+def test_guest_can_go_to_login_page(browser):
+    browser.get(link)
+    go_to_login_page(browser)
+    time.sleep(4)
