b50368a0860033e25550395043ef1e9a5d42e68b
Alex <hlbu@yandex.ru>
2021-10-17 02:34:58 +0300

    last edition

--- product_page.py
+++ product_page.py
@@ -1,10 +1,14 @@
+# -*- coding: utf-8 -*-
 import time
+
+from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
+
 class ProductPage:
-    def __init__(self,  browser, url=''):
+    def __init__(self, browser, url=''):
         self.browser = browser
         self.url = url
 
@@ -12,35 +16,50 @@ class ProductPage:
         self.browser.get(self.url)
 
     def should_be_add_to_cart_button(self):
-        add_button = self.browser.find_element_by_css_selector('.btn.btn-lg.btn-primary.btn-add-to-basket')
-        print(f'============>>>>{add_button.text}')
-        return add_button
+        try:
+            add_button = self.browser.find_element_by_css_selector('.btn.btn-lg.btn-primary.btn-add-to-basket')
+            return add_button
+        except NoSuchElementException as e:
+            print('Нет кнопки')
 
     def add_product_to_cart(self):
         self.should_be_add_to_cart_button().click()
 
     def should_be_success_message(self):
-        te = self.browser.find_element_by_css_selector('.alertinner')
-        print(te.text)
+        try:
+            self.browser.find_element_by_css_selector('.alertinner')
+        except NoSuchElementException as e:
+            print('Нет сообщения ')
 
     def go_to_writing_review(self):
-        # button = WebDriverWait(self.browser, 5).until(
-        #     EC.element_to_be_clickable((By.ID, "verify"))
-        # )
-        # button.click()
-        link_p = self.browser.find_element_by_xpath('//div[@id="reviews"]/following-sibling::p/a')
-        time.sleep(2)
+        link_p = WebDriverWait(self.browser, 5).until(
+            EC.element_to_be_clickable((By.ID, "write_review"))
+        )
         link_p.click()
-        time.sleep(2)
+        # link_p = self.browser.find_element_by_xpath('//div[@id="reviews"]/following-sibling::p/a')
 
     def check_that_there_is_name_of_the_goods(self):
-        pass
+        try: self.browser.find_element_by_css_selector \
+            ('.col-sm-6.product_main>h1').text
+        except NoSuchElementException:
+            print('No such text')
+            return False
 
     def check_that_there_is_price_of_the_goods(self):
-        pass
+        try: self.browser.find_element_by_css_selector\
+            ('.col-sm-6.product_main>p.price_color').text
+        except NoSuchElementException:
+            print('No such price')
+            return False
 
     def check_that_there_is_description_of_the_goods(self):
-        pass
+        try: self.browser.find_element_by_css_selector \
+            ('.product_page>p:nth-child(3)').text
+        except NoSuchElementException:
+            print('No such description')
+            return False
 
     def go_back_to_the_main_page(self):
-        pass
+        to_main_page = self.browser.find_element_by_css_selector('.col-sm-7.h1>a')
+        to_main_page.click()
+        time.sleep(5)
--- test_product_page.py
+++ test_product_page.py
@@ -10,13 +10,15 @@ def test_add_to_cart(browser):
     page.should_be_add_to_cart_button()   # проверяем что есть кнопка добавления в корзину
 
     page.add_product_to_cart()            # жмем кнопку добавить в корзину
-    # time.sleep(3)
 
-    page.should_be_success_message()      #alertinner   пр" был добавлен в вашу корзину. "оверяем что есть сообщение с нужным текстом
+    page.should_be_success_message()      #alertinner   проверяем что есть сообщение с нужным текстом
 
-    page.go_to_writing_review() # id="reviews" перейти к написанию отзыва
+    page.go_to_writing_review() # write_review перейти к написанию отзыва
 
-    page.check_that_there_is_name_of_the_goods() # проверьте, что есть наименование товара
+    page.check_that_there_is_name_of_the_goods() #проверьте, что есть наименование товара
+
     page.check_that_there_is_price_of_the_goods() # проверьте, что есть  цена товара
+
     page.check_that_there_is_description_of_the_goods() # проверьте, что есть описание товара
-    page.go_back_to_the_main_page() # Вернуться на главную страницу
+
+    page.go_back_to_the_main_page() #  class="col-sm-7 h1"Вернуться на главную страницу
